d8e2dcb04a325f38584e60154b0e54bab106a9f7
Dataa123 <diasamidzed53@gmail.com>
2025-01-21 22:06:50 +0400

    commit

--- Day 0/Homework/homework1and2.py
+++ Day 0/Homework/homework1and2.py
@@ -39,7 +39,7 @@ converter = {
     "U": 30, "V": 31, "W": 32, "X": 33, "Y": 34, "Z": 35
 }
 
-def hw2_try1(num, b):
+def hw2_try1(num, b): # works from 2 to 35 Position systems and converts all to decimal
     num = str(num)
     nums1 = []
     nums2 = []
@@ -63,15 +63,70 @@ def hw2_try1(num, b):
 
         return sum(nums1)
 
+# def hw2_try2(num, b, target):
+#     num = str(num)
+#     nums1 = []
+#     nums2 = []
+
+#     if "." in num:
+#         num = num.split(".")
+#         part1 = num[0][::-1]
+#         part2 = num[1]
+
+#         for i in range(len(part1)):
+#             nums1.append(int(converter[part1[i]]) * (2 ** i))
+
+#         for x in range(len(part2)):
+#             nums2.append(int(converter[part2[x]]) * (1 / 2 ** (x+1)))
+
+#         return sum(nums1 + nums2)
+#     else:
+#         num = num[::-1]
+#         for i in range(len(num)):
+#             nums1.append(int(converter[num[i]]) * (2 ** i))
+
+#     result = sum(nums1)
+
+#     result = str(result)
+#     nums1 = []
+#     nums2 = []
+
+#     if "." in num:
+#         num = num.split(".")
+#         part1 = num[0][::-1]
+#         part2 = num[1]
+
+#         for i in range(len(part1)):
+#             nums1.append(int(converter[part1[i]]) * (target ** i))
+
+#         for x in range(len(part2)):
+#             nums2.append(int(converter[part2[x]]) * (1 / target ** (x+1)))
+
+#         return sum(nums1 + nums2)
+#     else:
+#         num = num[::-1]
+#         for i in range(len(num)):
+#             nums1.append(int(converter[num[i]]) * (target ** i))
+        
+#     return sum(nums1 + nums2)
+# Failed. ???????????????
+
 # ალგორითმი ნასწავლი და განხილულია homework.png'ში
 # test:
-print(hw2_try1(1001, 2))
-print(hw2_try1(1001, 4))
-print(hw2_try1(520.3, 10))
-print(hw2_try1(1001, 16))
-print(hw2_try1(1011, 16))
-print(hw2_try1("1FFF", 16))
-print(hw2_try1("FFF", 16))
-print(hw2_try1(520.3, 6))
-# ✔
+print(hw2_try1(1001, 2)) # 9
+print(hw2_try1(1001, 4)) # 65
+print(hw2_try1(520.3, 10)) # 520.3
+print(hw2_try1(1001, 16)) # 4097
+print(hw2_try1(1011, 16)) # 4113
+print(hw2_try1("1FFF", 16)) # 8191
+print(hw2_try1("FFF", 16)) # 4095
+print(hw2_try1(520.3, 6)) # 192.5
 
+# print(hw2_try2(1001, 2, 10)) # 9
+# print(hw2_try2(1001, 4, 12)) # 65
+# print(hw2_try2(520.3, 10)) # 520.3
+# print(hw2_try2(1001, 16)) # 4097
+# print(hw2_try2(1011, 16)) # 4113
+# print(hw2_try2("1FFF", 16)) # 8191
+# print(hw2_try2("FFF", 16)) # 4095
+# print(hw2_try2(520.3, 6)) # 192.5

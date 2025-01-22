735568cf7168d27a588766f5d840a7f864ad6a78
Dataa123 <diasamidzed53@gmail.com>
2025-01-22 11:28:02 +0400

    commit

--- Day 0/Homework/homework1and2.py
+++ Day 0/Homework/homework1and2.py
@@ -4,13 +4,13 @@ def hw1_try1(a, b, c):
     return len([i for i in range(a, b) if i % c == 0])
 
 def hw1_try2(a, b, c):
-    return (b - a) // c if a % c != 0 or (a % c == 0 and b % c == 0) else (b - a) // c + 1
+    return ((b + a % c) - a) // c if a % c != 0 or (a % c == 0 and b % c == 0) else ((b + a % c) - a) // c + 1
 
 def hw1_try2_easy_version(a, b, c):
     if a % c != 0 or (a % c == 0 and b % c == 0):
-        return (b - a) // c
+        return ((b + a % c) - a) // c
     else:
-        return (b - a) // c + 1
+        return ((b + a % c) - a) // c + 1
 
 # ალგორითმი ნაპოვნი და ჩამოყალიბებულია hw1.png ფაილში
 # test:
@@ -26,6 +26,7 @@ print(hw1_try1(2, 20, 2))
 print(hw1_try2(2, 20, 2))
 print(hw1_try2_easy_version(2, 20, 2))
 print("\n")
+print(hw1_try2_easy_version(20,40,11))
 # ✔
 
 # 2.
